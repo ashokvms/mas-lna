198983ab872093d1ec0e4a453c3c447e4d089f31
Ashok Meenakshi Sundaram <mashoksc@gmail.com>
2014-06-15 23:24:08 +0200

    Updated read csv

--- src/hmm/StockMarketPrediction.py
+++ src/hmm/StockMarketPrediction.py
@@ -1,18 +1,35 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Created on Sun Jun 15 22:42:04 2014
 
-This temporary script file is located here:
-/home/sau/.spyder2/.temp.py
+@author: saugata, ashok
 """
-import numpy as np
-import random
-#from sklearn import tree
-import os
-from IPython.core.display import Image
 
+headers = []
+ise_us = []
+sp = []
+dax = []
+ftse = []
+nikkei = []
+bovespa = []
+eu = []
+em = []
+features = []
 
 ###### reading data from file ##############
 data_from_file = [i.strip().replace(' ', '').split(',') for i in open("../../data/istanbul_stock.csv").readlines()]
 
-print data_from_file
+headers = data_from_file[0]
+
+for i in range(1,537):
+    ise_us = ise_us + [float(data_from_file[i][2])]
+    sp = sp + [float(data_from_file[i][3])]
+    dax = dax + [float(data_from_file[i][4])]
+    ftse = ftse + [float(data_from_file[i][5])]
+    nikkei = nikkei + [float(data_from_file[i][6])]
+    bovespa = bovespa + [float(data_from_file[i][7])]
+    eu = eu + [float(data_from_file[i][8])]
+    em = em + [float(data_from_file[i][9])]
+
+for i in range(0,536):
+    features.append([sp[i],dax[i],ftse[i],nikkei[i],bovespa[i],eu[i],em[i]])
